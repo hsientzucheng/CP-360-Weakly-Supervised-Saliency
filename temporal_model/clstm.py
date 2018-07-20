2346707a634f596001d9f7faa2d4618d3b775213
hsientzucheng <pups0707@yahoo.com.tw>
2018-07-21 04:21:40 +0800

    split cp

--- temporal_model/clstm.py
+++ temporal_model/clstm.py
@@ -1,4 +1,5 @@
 import pdb
+import os, sys
 import torch
 from torch import nn
 import torch.nn.functional as f
@@ -6,100 +7,14 @@ from torch.autograd import Variable
 import math
 import matplotlib.pyplot as plt
 from torch.nn.parameter import Parameter
+sys.path.append('..')
+from utils.CubePad import CubePad
 # Define some constants
 KERNEL_SIZE = 3
 #PADDING = KERNEL_SIZE // 2
 PADDING = 0
 
 CP = True
-class ZeroPad(nn.Module):
-    def __init__(self, pad=1, USE_GPU=True):
-        super(ZeroPad, self).__init__()
-        self.USE_GPU = USE_GPU
-        self.pad = pad
-
-    def forward(self, x):
-        pad_row = Variable(torch.FloatTensor(x.size(0), x.size(1), self.pad, x.size(3)).zero_()).cuda()
-        x = torch.cat((pad_row, x, pad_row), 2)
-        pad_col = Variable(torch.FloatTensor(x.size(0), x.size(1), x.size(2), self.pad).zero_()).cuda()
-        x = torch.cat((pad_col, x, pad_col), 3)
-        return x
-
-class CubePad(nn.Module):
-    def __init__(self, USE_GPU = True):
-        super(CubePad, self).__init__()
-        self.USE_GPU = USE_GPU
-
-    def flip(self, tensor):
-        idx = [i for i in range(tensor.size(1)-1, -1, -1)]
-        
-        if self.USE_GPU:
-            idx = Variable(torch.cuda.LongTensor(idx))
-        else:
-            idx = Variable(torch.LongTensor(idx))
-
-        inverted_tensor = tensor.index_select(1, idx)
-        return inverted_tensor
-
-    def forward(self, x):
-
-        f_back = x[0]
-        f_down = x[1]
-        f_front = x[2]
-        f_left = x[3]
-        f_right = x[4]
-        f_top = x[5]         
-
-    
-        #faces order: 123456 = bdflrt (back, bottom, front, left, right, top)
- 
-        '''
-        4 plates:  /  t  /
-                  |=====| r
-                 l| /   | / 
-                  |=====|/
-                     d
-        '''
-        _t12 = torch.cat([torch.unsqueeze(self.flip(f_top[:,0,:]),0), torch.unsqueeze(f_front[:,-1,:],0)],0)
-        _t123 = torch.cat([_t12, torch.unsqueeze(f_top[:,-1,:],0)],0)
-        _t1234 = torch.cat([_t123, torch.unsqueeze(f_top[:,:,0],0)],0)
-        _t12345 = torch.cat([_t1234, torch.unsqueeze(self.flip(f_top[:,:,-1]),0)],0)
-        _t123456 = torch.cat([_t12345, torch.unsqueeze(self.flip(f_back[:,0,:]),0)],0)
-
-        _d12 = torch.cat([torch.unsqueeze(self.flip(f_down[:,-1,:]),0), torch.unsqueeze(self.flip(f_back[:,-1,:]),0)],0)
-        _d123 = torch.cat([_d12, torch.unsqueeze(f_down[:,0,:],0)],0)
-        _d1234 = torch.cat([_d123, torch.unsqueeze(self.flip(f_down[:,:,0]),0)],0)
-        _d12345 = torch.cat([_d1234, torch.unsqueeze(f_down[:,:,-1],0) ],0)
-        _d123456 = torch.cat([_d12345, torch.unsqueeze(f_front[:,0,:],0)],0)
-
-        _l12 = torch.cat([torch.unsqueeze(f_right[:,:,-1],0), torch.unsqueeze(self.flip(f_left[:,-1,:]),0)],0)
-        _l123 = torch.cat([_l12, torch.unsqueeze(f_left[:,:,-1],0)],0)
-        _l1234 = torch.cat([_l123, torch.unsqueeze(f_back[:,:,-1],0)],0)
-        _l12345 = torch.cat([_l1234, torch.unsqueeze(f_front[:,:,-1],0)],0)
-        _l123456 = torch.cat([_l12345, torch.unsqueeze(f_left[:,0,:],0)],0)
-
-        _r12 = torch.cat([torch.unsqueeze(f_left[:,:,0],0), torch.unsqueeze(f_right[:,-1,:],0)],0)
-        _r123 = torch.cat([_r12, torch.unsqueeze(f_right[:,:,0],0)],0)
-        _r1234 = torch.cat([_r123, torch.unsqueeze(f_front[:,:,0],0)],0)
-        _r12345 = torch.cat([_r1234, torch.unsqueeze(f_back[:,:,0],0)],0)
-        _r123456 = torch.cat([_r12345, torch.unsqueeze(self.flip(f_right[:,0,:]),0)],0)
-        # 6 x c x w for each
-        
-        p_tr = torch.unsqueeze(torch.mul(torch.add(_t123456[:,:,-1],_r123456[:,:,0]),0.5),2)
-        p_tl = torch.unsqueeze(torch.mul(torch.add(_t123456[:,:,0],_l123456[:,:,0]),0.5),2)
-        p_dr = torch.unsqueeze(torch.mul(torch.add(_d123456[:,:,-1],_r123456[:,:,-1]),0.5),2)
-        p_dl = torch.unsqueeze(torch.mul(torch.add(_d123456[:,:,0],_l123456[:,:,-1]),0.5),2)
-        # 6 x c x 1 for each
-
-        _lp123456p = torch.cat([torch.cat([p_tl,_l123456],2), p_dl],2)
-        _rp123456p = torch.cat([torch.cat([p_tr,_r123456],2), p_dr],2)
-
-        t_out = torch.cat([torch.unsqueeze(_t123456,2),x],2)
-        td_out = torch.cat([t_out,torch.unsqueeze(_d123456,2)],2)
-        tdl_out = torch.cat([torch.unsqueeze(_lp123456p,3),td_out],3)
-        tdlr_out = torch.cat([tdl_out,torch.unsqueeze(_rp123456p,3)],3)
-        return tdlr_out
-
 
 class ConvLSTMCell(nn.Module):
     """
