6c56dc5db60a7bd97542818687f36902d853f008
hsientzucheng <pups0707@yahoo.com.tw>
2018-07-30 16:24:15 +0800

    merge license branch

--- static_model/run.sh
+++ static_model/run.sh
@@ -1,3 +1,3 @@
-#python dataset_feat_extractor.py --mode resnet50 --dir ../../dataset/360_Discovery_Rot/test --out ./static
-python dataset_feat_extractor.py --mode resnet50 --dir PATH/TO/YOUR/DATASET/360_Discovery/test --out ./static
+python dataset_feat_extractor.py --mode resnet50 --dir ../../dataset/360_Discovery_Rot/test --out ./static
+#python dataset_feat_extractor.py --mode resnet50 --dir PATH/TO/YOUR/DATASET/360_Discovery/test --out ./static
 
