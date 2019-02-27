6588985bca3c66b794efe6d22792e321d87c9e47
hsientzucheng <pups0707@yahoo.com.tw>
2019-02-27 18:48:10 +0800

    add temporal training

--- inference.sh
+++ inference.sh
@@ -1,7 +1,7 @@
 mkdir output
 
 cd static_model
-#python dataset_feat_extractor.py --mode resnet50 -om -of
+python dataset_feat_extractor.py --mode resnet50 -om -of -oi
 
 cd ../temporal_model
 python test_temporal.py --dir ../output/static_resnet50 --model CLSTM_model_released.pth --overlay
